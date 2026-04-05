3c51ce5c554bd28e6465864abdfd1e827f2d95c8
aganta05 <ashikaganta@gmail.com>
2026-04-05 12:30:03 -0400

    Create settings.js
    created the two settings tasks

---
+++ phase 2/settings.js
@@ -0,0 +1,23 @@
+let currency = "USD";
+
+function loadSettings() {
+    document.getElementById("currencyDisplay").textContent = currency;
+
+    document.getElementById("lowBalanceToggle").checked = false;
+    document.getElementById("billDueToggle").checked = false;
+}
+
+function toggleCurrency() {
+    currency = (currency === "USD") ? "EUR" : "USD";
+    document.getElementById("currencyDisplay").textContent = currency;
+}
+
+function saveSettings() {
+    const lowBalanceAlert = document.getElementById("lowBalanceToggle").checked;
+    const billDueAlert = document.getElementById("billDueToggle").checked;
+
+    console.log("Low balance:", lowBalance);
+    console.log("Bill due:", billDue);
+}
+
+loadSettings();
